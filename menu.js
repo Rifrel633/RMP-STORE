d0cba7e59932625ac9f5217127ef620a301986ea
Rifrel633 <kingsupreme633@gmail.com>
2026-01-09 12:34:11 +0100

    Delete menu.js

--- menu.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("menu-toggle");
-  const links = document.getElementById("nav-links");
-  btn.addEventListener("click", () => links.classList.toggle("active"));
-});
